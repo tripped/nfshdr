242644609e70771fdd1d3b6686e116c8725e36fb
Trip Volpe <trip.volpe@gmail.com>
2018-04-03 12:13:45 -0700

    Do some packet parsing!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate pcap;
+use pcap::Capture;
+
 fn main() {
-    println!("Hello, world!");
+
+    // XXX: yeah, yeah, yeah, use try!
+    let mut cap = Capture::from_raw_fd(0).unwrap();
+
+    while let Ok(packet) = cap.next() {
+        println!("received packet! {:?}", packet);
+    }
 }
