2e4bf3a7a233bdbdc8fae11bbaa234ee75e6cc48
Trip Volpe <trip.volpe@gmail.com>
2018-04-19 19:01:53 -0700

    Use hacky TCP sequence tracking to find RPC messages

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ extern crate pnet_packet;
 
 use pcap::Capture;
 use pnet_packet::{ipv4,tcp};
+use std::collections::HashMap;
 use std::mem;
 
-fn tcp_data(data: &[u8]) -> &[u8] {
+/// Given a u8 slice containing a reassembled Ethernet "packet", return
+/// a TcpPacket representing the inner TCP header, and a u8 slice spanning
+/// the segment data.
+fn tcp_data_from_reassembled_ethernet(data: &[u8])
+        -> (tcp::TcpPacket, &[u8]) {
     // We can skip decoding Ethernet, since the payload of an 802.3
     // layer 2 frame starts at offset 14. (Assuming there's no VLAN
     // tag... TODO: add a check for 802.1Q ethertype values 0x8100,
@@ -19,8 +24,110 @@ fn tcp_data(data: &[u8]) -> &[u8] {
     let tcp = tcp::TcpPacket::new(&data[tcp_offset..]).unwrap();
 
     // Ditto for TCP data offset.
-    let data_offset = tcp_offset + 4 * tcp.get_data_offset() as usize;
-    &data[data_offset..]
+    let segment_start = tcp_offset + 4 * tcp.get_data_offset() as usize;
+
+    // Ethernet requires that the payload length be at least 46 bytes
+    // (when 802.1Q tag is absent); shorter payloads are zero-padded.
+    // Thus, to figure out how long the TCP segment data *really* is,
+    // we must look at the IPv4 header.
+    let segment_end = ip4_offset + ip4.get_total_length() as usize;
+
+    (tcp, &data[segment_start..segment_end])
+}
+
+#[derive(Hash, Eq, PartialEq, Debug)]
+struct Conversation {
+    started: bool,
+    sequence: u32,
+    next: u32,
+}
+
+/// Extracts ONC-RPC messages from a stream of TCP goop.
+struct RpcStream {
+    /// Map of destination port -> (sequence, next_rpc_offset). This should be
+    /// a good enough state, assuming the input capture only includes one TCP
+    /// conversation, or only conversations between two IP addresses!
+    conversations: HashMap<u16, Conversation>,
+
+    /// 1KiB of buffer ought to be enough for anybody
+    buffer: [u8;1024],
+}
+
+impl RpcStream {
+    pub fn new() -> RpcStream {
+        RpcStream {
+            conversations: HashMap::new(),
+            buffer: [0;1024],
+        }
+    }
+
+    /// Return the Conversation the given TCP segment is associated with.
+    /// Creates it using the segment's current sequence number if it does
+    /// not already exist in our map.
+    fn conversation_for(&mut self, seg: &tcp::TcpPacket) -> &mut Conversation {
+        self.conversations
+            .entry(seg.get_destination())
+            .or_insert(Conversation{
+                started: false,
+                sequence: seg.get_sequence(),
+                next: seg.get_sequence(),
+            })
+    }
+
+    /// Shove another packet into the stream!
+    /// XXX: this assumes that `data` is a reassembled "ethernet" frame
+    /// containing one IP packet (currently just IPv4), as libpcap would
+    /// provide.
+    pub fn update(&mut self, i: u32, data: &[u8]) {
+        let (tcp, segment) = tcp_data_from_reassembled_ethernet(data);
+
+        // Find the conversation record for this end of the stream
+        let mut conv = self.conversation_for(&tcp);
+
+        // Before we've latched on to a known ONC-RPC message, we have to
+        // search for one. In general this is not easy; if we don't already
+        // know where we are in the RPC conversation, we have to look at
+        // every byte of every TCP segment and try to match it against what
+        // we think an ONC-RPC header should look like. For now, though, we
+        // assume that the first segment with a large enough payload contains
+        // an ONC-RPC header at offset 0. This is true in our test data, and
+        // should usually be true if you start the capture *before* beginning
+        // any RPC traffic.
+
+        // Update the state of the conversation, including if we've just
+        // detected the initial RPC message.
+        conv.sequence = tcp.get_sequence();
+        if !conv.started && segment.len() > 20 { // super crappy!
+            conv.started = true;
+            conv.next = conv.sequence;
+        }
+
+        if conv.started {
+            // Is the next RPC position inside this segment?
+            let start = conv.sequence;
+            let end = start + segment.len() as u32;
+            if conv.next >= start && conv.next < end {
+
+                let offset = (conv.next - start) as usize;
+                println!("Frame {} dst={} seq={} size={}",
+                        i, tcp.get_destination(), conv.sequence, segment.len());
+                println!("Next={}", conv.next);
+                println!("Segment contains RPC message at {}", offset);
+
+                let rpc_snippet = &segment[
+                    offset..std::cmp::min(offset+64, segment.len())];
+
+                println!("{:16.1x}", HexDump(rpc_snippet));
+
+                // XXX: ASSUMES THE ENTIRE RPC HEADER IS IN THIS SEGMENT!!
+                let rpc = OncRpcMessage::new(&segment[offset..]).unwrap();
+
+                let cur_size = rpc.fragment_length() + 4;
+                println!("Next RPC in {} bytes", cur_size);
+                conv.next += cur_size;
+            }
+        }
+    }
 }
 
 /// Extract a 32-bit network byte order value as a host order u32.
@@ -232,46 +339,10 @@ impl<'a> std::fmt::UpperHex for HexDump<'a> {
 fn main() {
     // XXX: yeah, yeah, yeah, use try!
     let mut cap = Capture::from_raw_fd(0).unwrap();
-
+    let mut stream = RpcStream::new();
     let mut i = 0;
     while let Ok(packet) = cap.next() {
-        // Unpack the delicious, meaty 5-layer burrito of NFS traffic.
-        // We have the application layer beef and chicken combo (NFS inside
-        // ONC-RPC), transport layer beans (TCP) blended with spicy network
-        // layer rice (IPv4), liberally covered in link layer guacamole
-        // (Ethernet). In this metaphor, the physical layer tortilla has
-        // already been opened for us.
-        let data = tcp_data(packet.data);
-
-        // WARNING! We play fast and loose here by attempting to interpret
-        // every TCP payload as the beginning of an ONC-RPC message, bailing
-        // if it looks like anything else. This is obviously wrong, but in
-        // order to do it correctly, we would need to reconstruct the TCP
-        // stream, and frankly, ain't nobody got time for that. (Literally.
-        // The slightest delay in pulling data off stdin could cause us to
-        // drop packets on capture. This problem can be solved, of course,
-        // e.g., by keeping separate buffers for raw packets and TCP data
-        // which are maintained by threads running on different CPUs.)
-
         i += 1;
-
-        if let Some(rpc) = OncRpcMessage::new(data) {
-
-            let mtype = match rpc.message_type() {
-                Some(OncRpcMessageType::Call) => "C",
-                Some(OncRpcMessageType::Reply) => "R",
-                None => "?",
-            };
-
-            let payload = rpc.payload().unwrap_or(&[]);
-            let payload = &payload[..std::cmp::min(64, payload.len())];
-
-            println!("{},{},{},{}\n{:16.1x}",
-                     i,
-                     mtype,
-                     rpc.xid(),
-                     rpc.looks_like_it_be_what_it_is(),
-                     HexDump(payload));
-        }
+        stream.update(i, packet.data);
     }
 }
